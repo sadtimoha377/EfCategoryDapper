27cda1b35dda9e13b9e3f5653f1b16f15ed1bf23
timofey123 <makartim112@gmail.com>
2026-05-25 20:58:50 +0300

    Crud project start

---
+++ EfOne/Data/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using EfOne.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EfOne.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+                "Server=(localdb)\\MSSQLLocalDB;Database=EfCategoryDb;Trusted_Connection=True;"
+            );
+        }
+    }
+}
---
+++ EfOne/Program.cs
@@ -0,0 +1,149 @@
+﻿using EfOne.Data;
+using EfOne.Models;
+
+bool isRunning = true;
+
+while (isRunning)
+{
+    Console.WriteLine("\n===== MENU =====");
+    Console.WriteLine("1 - Get all categories");
+    Console.WriteLine("2 - Add category");
+    Console.WriteLine("3 - Delete category");
+    Console.WriteLine("4 - Find by ID");
+    Console.WriteLine("5 - Find by name");
+    Console.WriteLine("6 - Update category");
+    Console.WriteLine("0 - Exit");
+
+    Console.Write("Your choice: ");
+    string? choice = Console.ReadLine();
+
+    using AppDbContext db = new();
+
+    switch (choice)
+    {
+        case "1":
+            {
+                var categories = db.Categories.ToList();
+
+                foreach (var category in categories)
+                {
+                    Console.WriteLine($"{category.Id} | {category.Name} | {category.Description}");
+                }
+
+                break;
+            }
+
+        case "2":
+            {
+                Console.Write("Name: ");
+                string? name = Console.ReadLine();
+
+                Console.Write("Description: ");
+                string? description = Console.ReadLine();
+
+                Category category = new()
+                {
+                    Name = name!,
+                    Description = description!,
+                    CreatedAt = DateTime.Now
+                };
+
+                db.Categories.Add(category);
+                db.SaveChanges();
+
+                Console.WriteLine("Category added!");
+                break;
+            }
+
+        case "3":
+            {
+                Console.Write("ID: ");
+                int id = int.Parse(Console.ReadLine()!);
+
+                var category = db.Categories.Find(id);
+
+                if (category != null)
+                {
+                    db.Categories.Remove(category);
+                    db.SaveChanges();
+
+                    Console.WriteLine("Category deleted!");
+                }
+                else
+                {
+                    Console.WriteLine("Category not found!");
+                }
+
+                break;
+            }
+
+        case "4":
+            {
+                Console.Write("ID: ");
+                int id = int.Parse(Console.ReadLine()!);
+
+                var category = db.Categories.Find(id);
+
+                if (category != null)
+                {
+                    Console.WriteLine($"{category.Id} | {category.Name}");
+                }
+                else
+                {
+                    Console.WriteLine("Category not found!");
+                }
+
+                break;
+            }
+
+        case "5":
+            {
+                Console.Write("Name: ");
+                string? name = Console.ReadLine();
+
+                var categories = db.Categories
+                    .Where(x => x.Name.Contains(name!))
+                    .ToList();
+
+                foreach (var category in categories)
+                {
+                    Console.WriteLine($"{category.Id} | {category.Name}");
+                }
+
+                break;
+            }
+
+        case "6":
+            {
+                Console.Write("ID: ");
+                int id = int.Parse(Console.ReadLine()!);
+
+                var category = db.Categories.Find(id);
+
+                if (category != null)
+                {
+                    Console.Write("New name: ");
+                    category.Name = Console.ReadLine()!;
+
+                    Console.Write("New description: ");
+                    category.Description = Console.ReadLine()!;
+
+                    db.SaveChanges();
+
+                    Console.WriteLine("Category updated!");
+                }
+                else
+                {
+                    Console.WriteLine("Category not found!");
+                }
+
+                break;
+            }
+
+        case "0":
+            {
+                isRunning = false;
+                break;
+            }
+    }
+}
